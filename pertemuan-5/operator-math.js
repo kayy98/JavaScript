4f530602e621ca8edbf85aeb0f2fc1fc20486649
Rafka <rafka0714@student.abudzar.sch.id>
2026-02-17 09:31:00 +0700

    JavaScript

---
+++ pertemuan-5/operator-math.js
@@ -0,0 +1,113 @@
+//*unary binary operand
+let numberOne=10;   
+let numberTwo=5;    
+let resultMath=4 + 10;
+//*operand terbagi 2.operand kiri & operand kanan
+//*operand Tunggal
+let numberTree=6;
+
+//*unary
+let numberFour=-6;
+
+
+//*binary
+let numberFive=10 + 5;  
+
+let numberSix=-7;
+
+//*Math
+//*Sisa bagi %
+
+console.log(15%3); 
+
+//*Exponensial (pangkat) **
+console.log(2**9);
+
+//!Penambahan string  dengan +
+let name='fulan';
+let lastName='iman';
+let age=100;
+
+console.log('My Name' + 'Aiman');
+console.log(name + lastName);
+console.log(6 + 6 +'2');
+console.log('10'*'2'-'6'+'3');
+console.log(+'10' + + '2');
+console.log(+'10' + +'1');
+console.log(2+4/2*3-6);
+
+let a=4;
+let b=5;
+let c=b;
+console.log(a+c);
+
+let e =3;
+let f =6;
+let g =(e=f+e)-4;
+console.log(g);
+
+let h=4,i=5;
+let j;
+j=5;
+let k, l,m,n;
+k=l=m=n=7;
+n=m=l=k;
+console.log(k);
+console.log(l);
+
+let q=6;
+
+ let s=6;
+    s--;
+    console.log(s);
+
+let=3;
+s--;
+s++;
+console.log(s);
+
+//*type
+let myVar2=null;
+console.log(typeof myVar2);
+
+
+
+//*latihan
+let  layanganFuad=5;
+
+let colorOne='merah';
+let colorTwo='biru';
+let colorThree='hijau';
+let colorFour='kuning';
+let colorFive='ungu';
+
+let masukGot=colorThree;
+let diCuri=colorFive;
+
+let rusak=2;
+let updateLayangFuad=layanganFuad-rusak;
+console.log(' jumlah Layangan fuad adalah '+layanganFuad);
+
+
+
+//*Tugas
+
+let icecreamraffa=3;
+    
+let iceOne= 'matcha';
+let iceTwo= 'chocolate';
+let iceThree= 'vanilla';
+
+let dikasih= iceTwo;
+
+
+
+
+
+    
+
+
+
+
+
+
---
+++ tugas-js3/script.js
@@ -0,0 +1,116 @@
+//*1
+
+let permenAna = 30;
+let permenBudi = 10;
+let updateOnePermenAna = permenAna - permenBudi;
+let updateTwoPermenAna = updateOnePermenAna * 2;
+
+console.log(updateTwoPermenAna);
+console.log(20 * 2);
+
+//*2
+
+let kucingAna = 2;
+let kucingOne = "ali";
+let kucingTwo = "hasan";
+
+console.log(kucingOne);
+console.log(kucingTwo);
+console.log("nama kucing ana " + kucingOne + " " + kucingTwo);
+
+//*3
+
+let belalang = "12";
+let nyamuk = "11";
+let angsa = 10;
+
+let hasilBelalangNyamuk = +belalang + nyamuk;
+console.log(hasilBelalangNyamuk);
+
+let hasilNyamukAngsa = +nyamuk + angsa;
+console.log(hasilNyamukAngsa);
+
+
+
+//*4    
+let hargaBukuFiksi = 5000;
+let hargaBukuNonFiksi = 7000;
+let hargaPena = 2000;
+
+let totalPerItemOne = hargaBukuFiksi * 5;
+let totalPerItemTwo = hargaBukuNonFiksi * 3; 
+let totalPerItemThree = hargaPena * 2;
+let totalHarga = totalPerItemOne + totalPerItemTwo + totalPerItemThree;
+
+console.log('totoal harga yang di keluarkan aisah' + ' ' + totalHarga);
+
+
+
+//*5
+let penjumlahan1 = 5 + 3 * 2;
+let penjumlahan2 = (5 + 3) * 2;
+console.log(penjumlahan1);
+
+
+console.log(penjumlahan2);
+
+
+//*6
+
+
+let kresek = 4;
+let kotak = 6;
+let ember = 5 + (kresek = kotak);
+
+console.log(kresek);
+console.log(kotak);
+console.log(ember);
+
+
+
+
+//*7
+
+
+let x, y, z;
+x = y = z = 5 + 10 + 15;
+console.log('nilai x adalah ' + x);
+console.log('nilai x adalah ' + y);
+console.log('nilai x adalah ' + z);
+
+
+
+//*8
+
+
+let money = 4;
+money *= 2;
+money *= 2;
+console.log(money);
+
+
+
+//soal 9
+let J = 5;
+J++;
+
+console.log(J);
+
+
+let K = 8;
+K--;
+
+console.log(K);
+
+
+//*10
+
+let room = 'hafs';
+let age = 18;
+let shoesNumber = '42';
+let married = false;
+
+console.log(typeof room);
+console.log(typeof age);
+console.log(typeof shoesNumber);
+console.log(typeof married);
