b56003c0d76365e8d3892e00b8f7481269f6fef5
Rafka <rafka0714@student.abudzar.sch.id>
2026-05-14 13:28:38 +0700

    update

--- tugas-36Logik/script.js
+++ tugas-36Logik/script.js
@@ -1,10 +1,43 @@
-// jawaban nomor 1 
-let nilaiMtk = 91;
+//jawaban No. 1
+let nilaiMatimatika = 91;
 let nilaiIpa = 81;
-let nilaiIps = 91; 
-if (nilaiMtk >= 91 && nilaiIpa >= 91 && nilaiIps >= 91) {
- console.log("andi lulus"); 
- } else 
-{ console.log("andi tidak lulus");
-  
- }
+let nilaiIps = 91;
+if (nilaiMatimatika >= 91 && nilaiIpa >= 91 && nilaiIps >= 91) {
+  console.log("Andi Lulus");
+} else {
+  console.log("Andi Tidak Lulus");
+}
+//*jawaban No.2
+let niliamtk = 91;
+let nilaiIpa = 81;
+let nilaiIps = 91;
+if (niliamtk >= 91 || nilaiIpa >= 91 || nilaiIps >= 91) {
+  console.log("andi lulus");
+} else {
+  console.log("andi tidak lulus");
+}
+//*jawaban No.3
+let isLOGGEDIN = true;
+if (isLOGGEDIN) {
+  console.log("bisa login");
+} else {
+  console.log("tidak bisa login");
+}
+//*jawabn No.4
+
+let nilaiMtk = 80;
+let nilaiIpa = 80;
+
+if (nilaiMtk >= 80 && nilaiIpa >= 80) {
+  console.log("budi lulus");
+} else {
+  console.log("budi tidak lulus");
+}
+//jawaban NO.5
+let totalBelanja = 13000;
+let member = true;
+if (totalBelanja >= 10000 || member) {
+  console.log("anda mendapatkat diskon");
+} else {
+  console.log("anda tidak mendapat diskon");
+}
--- tugas-37/script.js
+++ tugas-37/script.js
@@ -12,4 +12,50 @@ let teh=null;
 let kopi=undefined;
 let susu=10;
 console.log(teh??kopi??susu);
-//*jawaban nya :yang muncul adalah let susu
+//*jawaban nya :yang muncul adalah let susu t
+
+let number=1;
+
+while (number<5){
+    console.log(number);
+    number++;     
+}
+while (number<=7        ){
+    console.log(number);
+    number++;     
+}
+
+let age=2;
+while (age<=10){
+ console.log(age);
+age+=3;
+ }
+
+
+
+ //!perulangan "do while"
+ let numberDo=1;
+
+ do{
+    console.log(numberDo);
+    numberDo++;
+
+    
+ }while (numberDo<=5)
+
+
+ let numberDO2=7;
+ do{
+    console.log(numberDO2);
+    numberDO2++;
+ }while(numberDO2<7);
+
+
+ 
+ for (let i=1; i<=5; i++){
+    if (i===4)continue;
+    console.log(i);
+ }  
+
+
+ 
